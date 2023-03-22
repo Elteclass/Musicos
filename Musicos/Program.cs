397770e384ec157fef167ce26922c43efc2cfa5f
Jaime Alvarez <126440030+Elteclass@users.noreply.github.com>
2023-03-22 19:58:41 +0000

    musicos

---
+++ Musicos/Program.cs
@@ -0,0 +1,70 @@
+﻿//clase Musico
+class Musico
+{
+    //Atributos
+    public string Nombre {get; set;}
+    
+    //Constructor
+    public Musico (string n)
+    {
+        Nombre = n;
+    }
+
+    //Método
+    public void Saludar()
+    {
+        Console.WriteLine("Hola soy {0}", Nombre);
+    }
+    public virtual void Toca()
+    {
+        Console.WriteLine($"{Nombre}, esta tocando su instrumento");
+    }
+
+}
+
+//clase bajista
+class Bajista:Musico
+{
+    public string Bajo {get; set;}
+
+    //Constructor
+    public Bajista(string n, string bajo):base(n)
+    {
+        Bajo = bajo;
+    }
+
+    //Métodos
+    public override void Toca()
+    {
+        Console.WriteLine($"{Nombre} tocando su {Bajo}");
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Lista
+        List<Musico> grupo = new List<Musico>();
+        grupo.Add(new Bajista("Maria", "Fluta"));
+        grupo.Add(new Musico ("Pancho"));
+
+        Console.WriteLine("Los integrantes del grupo son los siguientes:");
+
+        foreach ( var m in grupo )
+        {
+            m.Saludar();
+            m.Toca();
+        }
+
+        Console.WriteLine("Los Musicos son:");
+
+        Musico y = new Musico("Pancho");
+        y.Saludar();
+        y.Toca();
+        Bajista x = new Bajista("Maria", "Flauta");
+        x.Toca();
+    }
+}
+
+// Poner baterista y guitarrista
